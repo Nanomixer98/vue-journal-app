9dad5b077afcaf70477de55d4b72103edb68080f
alexisnava-stori <nasabmx@hotmail.com>
2022-11-30 14:46:56 -0600

    Added parameters to all journal api requests

--- src/api/journalApi.js
+++ src/api/journalApi.js
@@ -4,5 +4,13 @@ const journalApi = axios.create({
     baseURL: 'https://vue-demos-407f1-default-rtdb.firebaseio.com'
 })
 
+journalApi.interceptors.request.use((config) => {
+    config.params = {
+        auth: localStorage.getItem('idToken')
+    }
+
+    return config
+})
+
 // console.log(process.env.NODE_ENV); // Test during testing
 export default journalApi
